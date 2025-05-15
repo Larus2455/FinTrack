1aca2c17a632f25ae4b6323b3301dce47aa2b4ae
Larus2455 <137510455+Larus2455@users.noreply.github.com>
2025-05-15 08:09:57 +0400

    Add files via upload

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,31 @@
+﻿using FinTrack.Models;
+using Microsoft.AspNetCore.Mvc;
+
+public class SearchController : Controller
+{
+    private readonly ApplicationDbContext _context;
+    public SearchController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index(string query)
+    {
+        if (string.IsNullOrWhiteSpace(query))
+            return View();
+
+        // Пример поиска по категориям и транзакциям
+        var categories = _context.Categories
+            .Where(c => c.Title.Contains(query))
+            .ToList();
+
+        var transactions = _context.Transactions
+            .Where(t => t.Note.Contains(query))
+            .ToList();
+
+        ViewBag.Categories = categories;
+        ViewBag.Transactions = transactions;
+        ViewBag.Query = query;
+        return View();
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+// wwwroot/js/site.js
+$('#notificationsPanel').on('show.bs.offcanvas', function () {
+    // Здесь AJAX-запрос на сервер для получения уведомлений
+    // и обновление содержимого #notificationsList
+});
